ed0b7ff52a1bd57838096d625c58f75bd5e77918
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-08-11 22:43:43 +0530

    fourth commit

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ count=3
 outlist=[]
 for i in range(0,3):
   outlist.append(circular_linked_list[i])
-for i in range(3,length_of_circular_linked_list,2):
+for i in range(5,length_of_circular_linked_list,3):
   
   if circular_linked_list[0]!=circular_linked_list[i]:
     count+=1
