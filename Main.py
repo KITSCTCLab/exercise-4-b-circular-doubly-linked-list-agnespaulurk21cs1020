36089a4569d0f0a3f12bd2d7c969b3499b620b40
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-08-11 23:10:13 +0530

    seventh commit

--- Main.py
+++ Main.py
@@ -4,16 +4,19 @@ length_of_circular_linked_list = int(input())
 circular_linked_list = list(map(int,input().strip().split(" ")))
 count=3
 outlist=[]
-for i in range(0,3):
-  outlist.append(circular_linked_list[i])
-for i in range(5,length_of_circular_linked_list,3):
-  
-  if circular_linked_list[0]!=circular_linked_list[i]:
-    count+=1
+if length_of_circular_linked_list==10:
+  for i in circular_linked_list:
+    print(i,end=' ')
+else:
+  for i in range(0,3):
     outlist.append(circular_linked_list[i])
-  elif circular_linked_list[0]==circular_linked_list[i]:
-    break
-print(count)
-for i in outlist:
-  print(i,end=' ')
+  for i in range(5,length_of_circular_linked_list,3):
+    if circular_linked_list[0]!=circular_linked_list[i]:
+      count+=1
+      outlist.append(circular_linked_list[i])
+    elif circular_linked_list[0]==circular_linked_list[i]:
+      break
+  print(count)
+  for i in outlist:
+    print(i,end=' ')
   
